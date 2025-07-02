d0dea13b2fd272001de8b67fa8497164d04bac35
Queentel <joiqueentel@gmail.com>
2025-07-02 10:46:12 +0300

    intro to js 2

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Write your solution here!
+// index.js
+
+// Initial cats array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// 1. Destructive functions
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// 2. Non-destructive functions
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+// Exporting (optional for Node environments or test runner)
+// module.exports = {
+//   cats,
+//   destructivelyAppendCat,
+//   destructivelyPrependCat,
+//   destructivelyRemoveLastCat,
+//   destructivelyRemoveFirstCat,
+//   appendCat,
+//   prependCat,
+//   removeLastCat,
+//   removeFirstCat
+// };
